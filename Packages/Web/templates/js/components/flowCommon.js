5ad7253a0561c5543e7c5882b2d4281cefe01103
Owen-Delisle <Owen37164@gmail.com>
2020-02-12 16:47:56 -0800

    Adding Tracks to Web Package. (#6)
    * copying Track and Animation classes into FlowCommon folder for HTML Template
    
    * adding track and animation scripts to index.html
    
    * editing timeline to use new track and animation classes
    
    * fixing indentation
    
    * rename layerAnimation.js.flt -> layers.flt
    
    * create layer.flt
    
    * Add elementForID method
    
    * Add elementIDForMask method
    
    * add comments to Track.js, Animation.js
    
    * create propertyQuerries.flp
    
    * update track.js.flt to create Track with all keyvalues
    
    * add querrySelector method for getting querrySelector based on property
    
    * handle border animations with new Track system
    
    * fix steps in Animation,js
    
    * build tracks for Contents gravity
    
    * handle grandient stop color animation with Track
    
    * minor changes
    
    * Create convenience functions for getting keyvalues times and timingFunctions
    
    * dont create track for path property of shape layers
    
    * fix elementIDForMask method
    
    * make createAnimations.js.flt
    
    * Copy over Track and Animations.js for all the HTML templates
    
    * load Animation and Track in html via scripts
    
    * use lower cased timline name for fill image id
    
    * signatures
    
    Co-authored-by: Jeremie Benhamron <jeremiebenhamron@gmail.com>

--- Packages/Web/templates/js/components/flowCommon.js
+++ Packages/Web/templates/js/components/flowCommon.js
@@ -1,6 +1,8 @@
 function createCommonScripts(commonFolderPath) {
   let sources = [
     `${commonFolderPath}/web-animations.min.js`,
+    `${commonFolderPath}/Animation.js`,
+    `${commonFolderPath}/Track.js`,
     `${commonFolderPath}/Player.js`,
     `${commonFolderPath}/ToggleButton.js`
   ];
