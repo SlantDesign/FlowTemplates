d00d6329976532c666643aad92b66aae321a38ae
-- C4 -- <travis@c4ios.com>
2021-11-15 15:42:20 -0800

    Add progress bar template (#163)

---
+++ iOS-ProgressBar.flood/Project/Sources/AppDelegate.swift
@@ -0,0 +1,11 @@
+// Made With Flow.
+//
+// DO NOT MODIFY, your changes will be lost when this file is regenerated.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
