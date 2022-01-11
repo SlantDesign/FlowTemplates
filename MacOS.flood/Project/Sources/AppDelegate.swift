e6cba32bd072bf908ed4c00a9940aa1bd8b9b920
Travis <travis@createwithflow.com>
2022-01-10 17:23:26 -0800

    Converting xcode project from iOS format to MacOS format

--- MacOS.flood/Project/Sources/AppDelegate.swift
+++ MacOS.flood/Project/Sources/AppDelegate.swift
@@ -1,8 +1,18 @@
-// Made with Flow.
+// Made With Flow
 
-import UIKit
+import Cocoa
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
 }
